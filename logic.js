6375387f4b854a27851382d71f9e0bfae3e224e7
Einsteinia <informationviakate2004@gmail.com>
2023-10-02 16:38:51 +0530

    Necessary changes

---
+++ logic.js
@@ -0,0 +1,44 @@
+// function showContent(content) {
+//     var contentDiv = document.getElementById('content');
+
+//     // Create a function to load content from an HTML file
+//     function loadContentFromFile(filename) {
+//         var xhr = new XMLHttpRequest();
+//         xhr.onreadystatechange = function() {
+//             if (xhr.readyState === 4 && xhr.status === 200) {
+//                 contentDiv.innerHTML = xhr.responseText;
+//             }
+//         };
+//         xhr.open('GET', filename, true);
+//         xhr.send();
+//     }
+
+//     // Use a switch statement to determine which content to load
+//     switch (content) {
+//         case 'Django':
+//             loadContentFromFile('Django.html');
+//             break;
+//         case 'Flask':
+//             loadContentFromFile('about.html');
+//             break;
+//         case 'contact':
+//             loadContentFromFile('contact.html');
+//             break;
+//         default:
+//             contentDiv.innerText = 'Content not found';
+//     }
+// }
+
+function showContent(contentFileName) {
+    var contentDiv = document.getElementById('content');
+    var xhr = new XMLHttpRequest();
+    
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            contentDiv.innerHTML = xhr.responseText;
+        }
+    };
+    
+    xhr.open('GET', contentFileName, true);
+    xhr.send();
+}
