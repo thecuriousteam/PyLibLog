ac07ed40069975605cfa2ecf3aec9abf105a1482
thecuriousteam <curiousdevelopers.in@gmail.com>
2023-10-04 13:25:27 +0530

    Minor Changes Fixed

--- src/layouts/sidebar/index.jsx
+++ src/layouts/sidebar/index.jsx
@@ -83,18 +83,20 @@ const Sidebar = () => {
         variants={Nav_animation}
         initial={{ x: isTabletMid ? -250 : 0 }}
         animate={open ? "open" : "closed"}
+        // sidebar color code (#E0E0E0)
         className=" bg-[#212121] text-white shadow-xl z-[999] max-w-[19rem]  w-[19rem] 
-            overflow-hidden md:relative fixed
-         h-screen "
-      >
-        <div className="flex-col pt-5 pl-5 pr-5 gap-2.5 font-medium border-b h-[150px] border-slate-300  bg-[#29b6f6]">
+            overflow-hidden md:relative fixed h-screen ">
+        <div className="flex-col pt-5 pl-5 pr-5 gap-2.5 font-medium border-b h-[150px] border-slate-300  bg-[#8800ff]">
           {/* <img
             src="https://img.icons8.com/color/512/firebase.png"
             width={45}
             alt=""
           /> */}
-          <h1 className="text-[22px] text-white whitespace-pre text-center pb-5">
-            PYLIBLOG
+          <h1
+            className="text-[22px] text-white whitespace-pre text-center pb-5"
+            style={{ fontFamily: "Poppins, sans-serif", fontWeight: "light" }}
+          >
+            PyLibLog
           </h1>
           <Search />
         </div>
@@ -118,8 +120,8 @@ const Sidebar = () => {
             <div className="flex-1 text-sm z-50  max-h-48 my-auto  whitespace-pre   w-full  font-medium  ">
               <div className="flex border-y border-slate-300 p-4 items-center justify-between">
                 <div>
-                  <p>Clear History</p>
-                  <small>No-cost $0/month</small>
+                  <p>PyLibLog</p>
+                  <small>From The Curious Community</small>
                 </div>
                 {/* <p className="text-teal-500 py-1.5 px-3 text-xs bg-teal-50 rounded-xl">
                   Upgrade
