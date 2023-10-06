2a94a211c567b8d9a9aa7dbefa31e8a27fd14984
Samrat <samratapr40@gmail.com>
2023-10-06 21:31:52 +0530

    chaos

--- src/Python_Interpreter/PythonInterpreter.jsx
+++ src/Python_Interpreter/PythonInterpreter.jsx
@@ -1,49 +1,9 @@
-import React, { useState } from "react";
-import MonacoEditor from "react-monaco-editor";
-import { initPyodide, runPython } from "./pyodide";
+import React from 'react'
 
 function PythonInterpreter() {
-  const [pythonCode, setPythonCode] = useState("");
-  const [output, setOutput] = useState("");
-
-  const editorOptions = {
-    language: "python",
-    theme: "vs-dark",
-  };
-
-  const executePython = async () => {
-    try {
-      const result = await runPython(pythonCode);
-      setOutput(result);
-    } catch (error) {
-      console.error(error);
-      setOutput("error");
-    }
-  };
-
-  const onChangeMonaco = () => {
-    (newValue) => setPythonCode(newValue)
-   }
-
   return (
-    <div>
-      <h3>PythonInterpreter</h3>
-      <MonacoEditor
-        width="800"
-        height="400"
-        language="python"
-        theme="vs-dark"
-        value={pythonCode}
-        options={editorOptions}
-        onChange={onChangeMonaco}
-      />
-      <button onClick={executePython}>Run</button>
-      <div>
-        <h3>Output:</h3>
-        <pre>{output}</pre>
-      </div>
-    </div>
-  );
+    <div>PythonInterpreter</div>
+  )
 }
 
-export default PythonInterpreter;
+export default PythonInterpreter
