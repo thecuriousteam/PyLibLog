75dcbeca41433c1805603378bf0ea99e479a39a4
harshita1611 <harshitabhamhani5@gmail.com>
2023-10-07 17:56:48 +0530

    completed matplotlib

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import AllApps from "./Python_Library_Pages/AllApps";
 import PythonBasics from "./Python_Library_Pages/Python_Basics/Introduction-to-Python";
 import NumpyBasics from "./Python_Library_Pages/Numpy/Intro-to-Numpy";
 import PandasBasics from "./Python_Library_Pages/Pandas/Intro-to-Pandas";
-
+import MatplotlibBasics from "./Python_Library_Pages/Matplotlib/Intro-to-Matplotlib";
 
 const App = () => {
   return (
@@ -16,19 +16,24 @@ const App = () => {
         <Route path="/Python-Basics" element={<Outlet />}>
           <Route path="Introduction-to-Python" element={<PythonBasics />} />
         </Route>
+
         <Route path="/NumPy-Library" element={<Outlet />}>
-        <Route path="Intro-to-Numpy" element={<NumpyBasics />} />
+          <Route path="Intro-to-Numpy" element={<NumpyBasics />} />
           {/* if we have child element we need added here */}
           {/* <Route index element={<Two />} />I
           <Route path="Introduction-to-Conditions" element={<Five />} />
           <Route path="Loops-in-Python" element={<Six />} />
           <Route path="While-Loop-in-Python" element={<Seven />} /> */}
         </Route>
+
         <Route path="Pandas-Library" element={<Outlet />}>
-        <Route path="Intro-to-pandas" element={<PandasBasics />} />
+          <Route path="Intro-to-pandas" element={<PandasBasics />} />
           {/* <Route index element={<Two />} />
           <Route path="Inbuilt-Functions" element={<Inbuilt />} />
           <Route path="Loops-in-Python" element={<User />} /> */}
+        </Route>
+        <Route path="Matplotlib-Library" element={<Outlet/>}>
+          <Route path="Intro-to-Matplotlib" element={<MatplotlibBasics/>}/>
         </Route>
         {/* remaing routes*/}
       </Routes>
