bd41159ded9a1327c05d906427393a165ce9cf9c
shrey802 <shreyashpingle752@gmail.com>
2023-10-08 15:12:41 +0530

    sidebar issue fixed

--- src/layouts/sidebar/index.jsx
+++ src/layouts/sidebar/index.jsx
@@ -37,93 +37,96 @@ const Sidebar = () => {
 
   const Nav_animation = isTabletMid
     ? {
-        open: {
-          x: 0,
-          width: "16rem",
-          transition: {
-            damping: 40,
-          },
+      open: {
+        x: 0,
+        width: "16rem",
+        transition: {
+          damping: 40,
         },
-        closed: {
-          x: -250,
-          width: 0,
-          transition: {
-            damping: 40,
-            delay: 0.15,
-          },
+      },
+      closed: {
+        x: -250,
+        width: 0,
+        transition: {
+          damping: 40,
+          delay: 0.15,
         },
-      }
+      },
+    }
     : {
-        open: {
-          width: "24rem",
-          transition: {
-            damping: 40,
-          },
+      open: {
+        width: "24rem",
+        transition: {
+          damping: 40,
         },
-        closed: {
-          width: "4rem",
-          transition: {
-            damping: 40,
-          },
+      },
+      closed: {
+        width: "4rem",
+        transition: {
+          damping: 40,
         },
-      };
-// subMenusList.map(menu => {
-//   console.log(menu);
-// })
+      },
+    };
+  // subMenusList.map(menu => {
+  //   console.log(menu);
+  // })
+  const styleSidebar = {
+    "position": "sticky",
+    "top": "0",
+    "maxHeight": "100vh"
+  }
   return (
-    <div>
+    <div style={styleSidebar}>
       <div
         onClick={() => setOpen(false)}
-        className={`md:hidden fixed inset-0 max-h-screen z-[998] bg-black/50 ${
-          open ? "block" : "hidden"
-        } `}
+        className={`md:hidden fixed inset-0 max-h-screen z-[998] bg-black/50 ${open ? "block" : "hidden"
+          } `}
       ></div>
       <motion.div
         ref={sidebarRef}
         variants={Nav_animation}
         initial={{ x: isTabletMid ? -250 : 0 }}
         animate={open ? "open" : "closed"}
+        // sidebar color code (#E0E0E0)
         className=" bg-[#212121] text-white shadow-xl z-[999] max-w-[19rem]  w-[19rem] 
-            overflow-hidden md:relative fixed
-         h-screen "
-      >
-        <div className="flex-col pt-5 pl-5 pr-5 gap-2.5 font-medium border-b h-[150px] border-slate-300  bg-[#29b6f6]">
+            overflow-hidden md:relative fixed h-screen ">
+        <div className="flex-col pt-5 pl-5 pr-5 gap-2.5 font-medium border-b h-[150px] border-slate-300  bg-[#8800ff]">
           {/* <img
             src="https://img.icons8.com/color/512/firebase.png"
             width={45}
             alt=""
           /> */}
-          <h1 className="text-[22px] text-white whitespace-pre text-center pb-5">LEARN DOCKER</h1>
-         <Search />
+          <h1
+            className="text-[22px] text-white whitespace-pre text-center pb-5"
+            style={{ fontFamily: "Poppins, sans-serif", fontWeight: "light" }}
+          >
+            PyLibLog
+          </h1>
+          <Search />
         </div>
-        
+
         <div className="flex flex-col  h-full">
           <ul className="whitespace-pre px-2.5 text-[0.9rem] py-5 flex flex-col gap-1  font-medium overflow-x-hidden scrollbar-thin scrollbar-track-[#ccc] scrollbar-thumb-yellow-500   md:h-[68%] h-[70%]">
-           
             {(open || isTabletMid) && (
               <div className="py-5 border-slate-300 ">
                 {/* <small className="pl-3 text-slate-500 inline-block mb-2">
                   Product categories
                 </small> */}
-                {subMenusList?.map((data,i) => (
+                {subMenusList?.map((data, i) => (
                   <div key={i} className="flex flex-col gap-1">
                     <SubMenu data={data} />
                   </div>
                 ))}
               </div>
             )}
-           
           </ul>
           {open && (
-            <div className="flex-1 text-sm z-50  max-h-48 my-auto  whitespace-pre   w-full  font-medium  ">
-              <div className="flex border-y border-slate-300 p-4 items-center justify-between">
+            <div className="flex-1 text-sm z-50 max-h-48 my-auto whitespace-pre w-full font-medium mb-0">
+              <div className="flex border-y border-slate-300 items-center justify-between">
                 <div>
-                  <p>Clear History</p>
-                  <small>No-cost $0/month</small>
+                  <p>PyLibLog</p>
+                  <small>From The Curious Community</small>
                 </div>
-                {/* <p className="text-teal-500 py-1.5 px-3 text-xs bg-teal-50 rounded-xl">
-                  Upgrade
-                </p> */}
               </div>
             </div>
           )}
@@ -135,15 +138,15 @@ const Sidebar = () => {
           animate={
             open
               ? {
-                  x: 0,
-                  y: 0,
-                  rotate: 0,
-                }
+                x: 0,
+                y: 0,
+                rotate: 0,
+              }
               : {
-                  x: -10,
-                  y: -200,
-                  rotate: 180,
-                }
+                x: -10,
+                y: -200,
+                rotate: 180,
+              }
           }
           transition={{ duration: 0 }}
           className="absolute w-fit h-fit md:block z-50 hidden right-2 bottom-3 cursor-pointer"
