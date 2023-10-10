0877132fffe62fbc47874f15b353536bd80b802d
asher <111245656+asherantony@users.noreply.github.com>
2023-10-10 20:02:36 +0530

    Templates for Matplotlib, Seaborn, Scikit-Learn
    I added JSX template code for the Matplotlib, Seaborn and Scikit-Learn folders.

---
+++ src/Python_Library_Pages/Scikit-Learn/Intro-to-ScikitLearn.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ScikitBasics = () => {
+  return (
+    <div>
+      <h1>Introduction to ScikitLearn</h1>
+
+      <p>Development can start for ScikitLearn from here</p>
+    </div>
+  );
+};
+
+export default ScikitBasics;
---
+++ src/Python_Library_Pages/Seaborn/Intro-to-Seaborn.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const SeabornBasics = () => {
+  return (
+    <div>
+      <h1>Introduction to Seaborn</h1>
+
+      <p>Development can start for Seaborn from here</p>
+    </div>
+  );
+};
+
+export default SeabornBasics;
