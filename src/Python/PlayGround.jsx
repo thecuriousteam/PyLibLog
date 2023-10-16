64b2a45b5aea4add70de302a18d2e55c3d1cf1f4
samrat <samratapr40@gmail.com>
2023-10-16 19:47:11 +0530

    fixing pyp

--- src/Python/PlayGround.jsx
+++ src/Python/PlayGround.jsx
@@ -1,5 +1,4 @@
 import React,{ useState } from 'react';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
 import Scripts from './Scripts';
 import CodeEditor from './CodeEditor';
 import "brace/mode/python";
